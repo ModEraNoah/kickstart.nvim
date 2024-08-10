c502714bf50688db8565e72684721aa158cca768
ModEraNoah <klauck02@outlook.com>
2024-08-10 22:51:41 +0200

    formatting options

--- init.lua
+++ init.lua
@@ -659,7 +659,7 @@ require('lazy').setup({
     cmd = { 'ConformInfo' },
     keys = {
       {
-        '<leader>f',
+        '<leader>cf',
         function()
           require('conform').format { async = true, lsp_fallback = true }
         end,
@@ -686,6 +686,7 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        typescript = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
