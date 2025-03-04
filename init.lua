9290be04f5d6912d7d0e4bb3f371a2bb6749d1da
ModEraNoah <klauck02@outlook.com>
2025-03-04 18:07:58 +0100

    feat: add Oil.nvim

--- init.lua
+++ init.lua
@@ -1056,6 +1056,17 @@ require('lazy').setup({
       end)
     end,
   },
+
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+  },
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
